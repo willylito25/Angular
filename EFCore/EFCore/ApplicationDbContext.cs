dd5c6ae820e626d333abba0887f3ae4e495dc001
Willy Sarmiento <willygerman49@gmail.com>
2019-01-22 22:13:27 -0400

    Agregando Proyecto de consola

---
+++ EFCore/EFCore/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCore
+{
+    public class ApplicationDbContext : DbContext 
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-3CO6ER5;Initial Catalog=PruebaEfCoreConsola;Integrated Security=True")
+                .EnableSensitiveDataLogging(true)
+                .UseLoggerFactory(new LoggerFactory().AddConsole((category, level) => level == LogLevel.Information && category == DbLoggerCategory.Database.Command.Name, true));
+        }
+        public DbSet<Estudiante> Estudiantes { get; set; }
+    }
+}
---
+++ EFCore/EFCore/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EFCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Estudiante
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
